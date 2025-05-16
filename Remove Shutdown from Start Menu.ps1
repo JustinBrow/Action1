2775e158942fa8c013ade38911a10861a3cea133
Justin <46439389+JustinBrow@users.noreply.github.com>
2025-05-16 18:10:22 -0400

    Update Remove Shutdown from Start Menu.ps1

--- Remove Shutdown from Start Menu.ps1
+++ Remove Shutdown from Start Menu.ps1
@@ -1,9 +1,21 @@
-try
+$GPOsupportedSKUs = @(48, 161);
+$OS = Get-CimInstance -ClassName Win32_OperatingSystem;
+if ($OS.OperatingSystemSKU -in $GPOsupportedSKUs) # Only remove shutdown on SKUS that support GPOs. Don't have enterprise in out org so I didn't include it.
 {
-   Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideShutDown' -Name 'value' -Value 1
+   try
+   {
+      Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideShutDown' -Name 'value' -Value 1;
+      & 'C:\Windows\System32\powercfg.exe' /SETACVALUEINDEX SCHEME_CURRENT SUB_BUTTONS PBUTTONACTION 0;
+      & 'C:\Windows\System32\powercfg.exe' /SETDCVALUEINDEX SCHEME_CURRENT SUB_BUTTONS PBUTTONACTION 0;
+   }
+   catch
+   {
+      $Host.UI.WriteErrorLine("Could not set registry value: $($_.Exception.Message)");
+      exit 2;
+   };
 }
-catch
+else
 {
-   $Host.UI.WriteErrorLine("Could not set registry value: $($_.Exception.Message)")
-   exit 1
-}
+   $Host.UI.WriteErrorLine("Unsupported Windows edition: $($OS.OperatingSystemSKU)");
+   exit 1;
+};
