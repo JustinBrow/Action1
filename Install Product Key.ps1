7457a25646c1df01b889f8b0f37ba6ae5f824038
Justin <46439389+JustinBrow@users.noreply.github.com>
2025-10-24 22:34:03 -0400

    Update Install Product Key.ps1

--- Install Product Key.ps1
+++ Install Product Key.ps1
@@ -1,6 +1,6 @@
 if (${ProductKey})
 {
-# The "| Out-Null" is necessary unless you want your product key stored in the automation history.
+<# The "| Out-Null" is necessary unless you want your product key stored in the automation history. #>
    & 'C:\Windows\System32\cscript.exe' C:\Windows\System32\slmgr.vbs /ipk ${ProductKey} | Out-Null;
    & 'C:\Windows\System32\cscript.exe' C:\Windows\System32\slmgr.vbs /ato;
    & 'C:\Windows\System32\cscript.exe' C:\Windows\System32\slmgr.vbs /dlv;
