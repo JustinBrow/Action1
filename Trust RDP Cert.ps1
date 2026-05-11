ec87ebef3c17d1e7bbc33812bb630d8157cd26b2
Justin <46439389+JustinBrow@users.noreply.github.com>
2026-05-11 14:25:48 -0400

    Update Trust RDP Cert.ps1

--- Trust RDP Cert.ps1
+++ Trust RDP Cert.ps1
@@ -1,19 +1,40 @@
-Get-ChildItem -Path 'Registry::HKEY_USERS\' | where PSChildName -ne '.DEFAULT' | ForEach {
-   if (Test-Path -Path ([IO.Path]::Combine($_.PSPath, 'Software\Microsoft\Terminal Server Client'))) {
-      Get-Item ([IO.Path]::Combine($_.PSPath, 'Software\Microsoft\Terminal Server Client')) | New-ItemProperty -Name 'RdpLaunchConsentAccepted' -Type DWord -Value 1 -Force | Out-Null;
-   }
-};
 try {
-   $hostname = 'https://rdsh.example.com/'
-   $path = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services';
-   $WR = Invoke-WebRequest -UseBasicParsing -Uri $hostname -TimeoutSec 3 -ErrorAction Stop;
-   $servicePoint = [Net.ServicePointManager]::FindServicePoint($hostname);
-   $certHash = $servicePoint.Certificate.GetCertHashString([Security.Cryptography.HashAlgorithmName]::SHA1);
-   if (-not(Test-Path $path)) {
-      New-Item -Path $path -Force -ErrorAction Stop | Out-Null;
+   Get-ChildItem -Path 'Registry::HKEY_USERS\' | where PSChildName -ne '.DEFAULT' | ForEach {
+      if (Test-Path -Path ([IO.Path]::Combine($_.PSPath, 'Software\Microsoft\Terminal Server Client'))) {
+         Get-Item ([IO.Path]::Combine($_.PSPath, 'Software\Microsoft\Terminal Server Client')) | New-ItemProperty -Name 'RdpLaunchConsentAccepted' -Type DWord -Value 1 -Force | Out-Null;
+      };
    };
-   New-ItemProperty -Path $path -Name 'TrustedCertThumbprints' -Value $certHash -PropertyType String -Force -ErrorAction Stop | Out-Null;
 } catch {
    $Host.UI.WriteErrorLine($_.Exception.Message);
-   exit 1;
+};
+.{
+   try {
+      $hostname = 'https://rdsh.example.com/'
+      $path = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services';
+      $WR = Invoke-WebRequest -UseBasicParsing -Uri $hostname -TimeoutSec 3 -ErrorAction Stop;
+      $servicePoint = [Net.ServicePointManager]::FindServicePoint($hostname);
+      $certHash = $servicePoint.Certificate.GetCertHashString([Security.Cryptography.HashAlgorithmName]::SHA1);
+      if (-not (Test-Path $path)) {
+         New-Item -Path $path -Force -ErrorAction Stop | Out-Null;
+      };
+      if (Get-ItemProperty -Path $path -Name 'TrustedCertThumbprints' -ErrorAction SilentlyContinue) {
+         if ($Host.Version.Major -le 4) {
+            throw [NotSupportedException]::new($Host.Version);
+         };
+         $TrustedCertThumbprints = Get-ItemPropertyValue -Path $path -Name 'TrustedCertThumbprints';
+         if ($TrustedCertThumbprints.Split(',').Contains($certHash)) {
+            return;
+         };
+         if ($TrustedCertThumbprints.Split(',').Count -eq 2) {
+            $TrustedCertThumbprints = $TrustedCertThumbprints.Split(',')[0];
+         };
+         $TrustedCertThumbprints = $certHash, $TrustedCertThumbprints -join ',';
+         Set-ItemProperty -Path $path -Name 'TrustedCertThumbprints' -Value $TrustedCertThumbprints -Force -ErrorAction Stop | Out-Null;
+      } else {
+         New-ItemProperty -Path $path -Name 'TrustedCertThumbprints' -Value $certHash -PropertyType String -Force -ErrorAction Stop | Out-Null;
+      };
+   } catch {
+      $Host.UI.WriteErrorLine($_.Exception.Message);
+      exit 1;
+   };
 };
